fbe88c2afa397b41c1eacb0853aecf7c4f94313f
mario <mariovr5@hotmail.com>
2024-12-09 16:23:20 +0100

    clean part 1 day9

--- 9.py
+++ 9.py
@@ -9,16 +9,8 @@
 # -*- coding: utf-8 -*-
 #! /usr/bin/env python 
 
-import os
-import sys
-import re
-
 def main(args , **kwargs):
-
-    print(args[0])
-          
     newstr = []
-    print(args[0])
     args[0] += str(0)
     for j, tup in enumerate([(int(args[0][i]) , int(args[0][i+1])) for i in range(0,len(args[0])-1,2) ]):
         for n in range(tup[0]):
@@ -26,33 +18,18 @@ def main(args , **kwargs):
         for m in range(tup[1]):
             newstr.append('.')
 
-    print(newstr)
     revstring = [i for i in reversed(newstr) ]
-    #print(revstring)
-    print(len(newstr))
-    print(len(args[0]))
-    #return 0
     breakreach = False
     for i in revstring:
+        if '.' not in  newstr:
+            break
         if i != '.':
-            #print(newstr)
-            try:
-                index = newstr.index('.')
-                #print(index)
-            except ValueError:
-                breakreach = True
-                break
-            if index > 0:
-                newstr = newstr[:index] + [i] + newstr[index+1:len(newstr) ]
-        if not breakreach:
-            newstr = newstr[:-1]
-        #print(newstr)
-    newstr += [5258] *5
-    print(newstr)
+            index = newstr.index('.')
+            newstr = newstr[:index] + [i] + newstr[index+1:len(newstr) ]
+        newstr = newstr[:-1]
     result = 0
     for i, num in enumerate(newstr):
         result += i* int(num)
-
     return result
 
 if __name__ == "__main__":
@@ -61,7 +38,7 @@ if __name__ == "__main__":
 
     lines = [line.strip() for line in stringlist.strip().split('\n')]
     print(lines)
-    #assert main(lines) == 1928
+    assert main(lines) == 1928
 
     file = "input9.txt"
     with open(file,'r') as f:
