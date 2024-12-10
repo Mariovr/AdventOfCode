3e9d86bc32f043ce75c27d4e61c96bc1a5cbf6db
mario <mariovr5@hotmail.com>
2024-12-10 08:06:34 +0100

    added part 2 + cleaning

--- 10.py
+++ 10.py
@@ -9,21 +9,10 @@
 # -*- coding: utf-8 -*-
 #! /usr/bin/env python 
 
-import os
 import sys
 sys.setrecursionlimit(999999999)
-from copy import deepcopy
-import re
 
-class Game(object):
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        outputstr = 'Value: ' + str(self.value) + '\n'
-        return outputstr
-
-def move(pos , dmap, xdim , ydim, nset):
+def move(pos , dmap, xdim , ydim, startc, endset):
     stepl = [(-1,0), (0,1),(1,0),(0,-1) ]
     npos = (0,0)
     oldval = dmap[pos[0] ] [pos[1] ]
@@ -33,19 +22,16 @@ def move(pos , dmap, xdim , ydim, nset):
             nval =dmap[npos[0] ] [npos[1]] 
             if  nval == (oldval +1):
                 if nval != 9:
-                    nset.update( move(npos , dmap, xdim,ydim,nset))
+                    startc = move(npos , dmap, xdim,ydim, startc, endset)
                 else:
-                    print('9 found')
-                    nset.add(npos) 
-    return nset
+                    startc += 1
+                    endset.add(npos)
+    return startc
 
 def main(args , **kwargs):
-    result = 0
-
     dmap = []
     xdim = len(args)
     ydim = len(args[0])
-    stepl = [(-1,0), (0,1),(1,0),(0,-1) ]
 
     startp = []
     for i, line in enumerate(args):
@@ -56,12 +42,15 @@ def main(args , **kwargs):
             row.append(int(pos))
         dmap.append(row)
 
+    result1 = 0
+    result2 = 0
     for spos in startp:
-        print('startp: ' ,spos)
-        result += len(move(spos, dmap , xdim ,ydim ,set()))
-        print('result: ' , result)
-
-    return result
+        endset = set()
+        result2 += move(spos, dmap , xdim ,ydim ,0, endset)
+        result1 += len(endset)
+    print('Result 1:' , result1)
+    print('Result 2:' , result2)
+    return result2
 
 if __name__ == "__main__":
     stringlist ="""89010123
@@ -76,7 +65,7 @@ if __name__ == "__main__":
 
     lines = [line.strip() for line in stringlist.strip().split('\n')]
     print(lines)
-    assert main(lines) == 36
+    assert main(lines) == 81
 
     file = "input10.txt"
     with open(file,'r') as f:
