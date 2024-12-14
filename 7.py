160f6389aad526d1c39b6685de4e98001a87f2ab
mario <mariovr5@hotmail.com>
2024-12-14 13:39:24 +0100

    cleaning

--- 7.py
+++ 7.py
@@ -9,8 +9,6 @@
 # -*- coding: utf-8 -*-
 #! /usr/bin/env python 
 
-import os
-import sys
 import re
 from itertools import product
 
@@ -64,36 +62,26 @@ class Equation(object):
         return outputstr
 
 def main(args , **kwargs):
-    result = 0
+    results = [0,0]
     eqlist = []
     for i, line in enumerate(args):
         match = re.search('(\d+):\s([\s\d]+)', line)
         eqlist.append(Equation(int(match.group(1)) , [int(num) for num in match.group(2).split()]))
+        eqlist[i].check_valid()
+        if eqlist[i].valid:
+            results[0] += eqlist[i].result
+            eqlist[i].valid = False
         eqlist[i].check_valid2()
         if eqlist[i].valid:
-            result += eqlist[i].result
+            results[1] += eqlist[i].result
 
-    return result
+    print('Result 1 is: ', results[0])
+    print('Result 2 is: ', results[1])
+    return results
 
 if __name__ == "__main__":
-    stringlist ="""190: 10 19
-3267: 81 40 27
-83: 17 5
-156: 15 6
-7290: 6 8 6 15
-161011: 16 10 13
-192: 17 8 14
-21037: 9 7 18 13
-292: 11 6 16 20
-"""
-
-    lines = [line.strip() for line in stringlist.strip().split('\n')]
-    assert main(lines) == 11387
-
     file = "input7.txt"
     with open(file,'r') as f:
         lines = f.readlines()
         lines = [line.strip() for line in lines]
     result = main(lines)
-    print('Result is: ', result)
-
